550e51b3cc64f3174410785989b95fd2d248c387
Anders Semb Hermansen <anders.hermansen@gmail.com>
2024-05-01 20:49:52 +0200

    Simplify even more.

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,20 @@
-export default async function Home() {
+import { fetchArticles } from "@/data/fetch";
+
+export default async function Home({ params }: { params: { lang: string; } }) {
+  const data1 = await fetchArticles("no", 10);
+  const data2 = await fetchArticles("en", 10);
+
   return (
     <main>
-        <h1>Test</h1>
+        <h2>From route: locale {params.lang}</h2>
+        <h3>data1</h3>
+        <code>
+          {data1.length}
+        </code>
+        <h3>data2</h3>
+        <code>
+          {data2.length}
+        </code>
     </main>
   );
 }
