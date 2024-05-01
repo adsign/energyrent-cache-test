9427a2e81ecfabfbe8909b2a48ceb009d8c066c0
Anders Semb Hermansen <anders.hermansen@gmail.com>
2024-05-01 20:37:22 +0200

    Simplify reproduction steps.

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,3 +1,6 @@
+import { headers } from "next/headers";
+
 export default function NotFoundPage() {
+	const locale = headers().get('x-test-header') || 'en';
 	return (<><p>This is the 404 page inside lang</p></>);
 }
